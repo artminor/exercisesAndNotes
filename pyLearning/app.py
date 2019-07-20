56829b1a19561386d3f9383b91fc50fc37a6e226
Jun Chen <junchen07@gmail.com>
2019-07-19 23:20:01 -0400

    python class

--- pyLearning/app.py
+++ pyLearning/app.py
@@ -1,41 +1,43 @@
 from math import *
+#
+# character_name = "John"
+# character_age = 35
+#
+#
+# print(str(character_age))
+#
+# age = "35"
+#
+# print("There once was a man named " + character_name + ", ")
+# print("he was " + age + " years old. ")
+# print("He really liked the name "+character_name+", ")
+# print("but didn't like being " + age + " years old.")
+# print("      /|")
+# print("     / |")
+# print("    /  |")
+# print("   /___|")
+#
+# print(character_name.upper())
+# print(character_name.isupper())
+# print(len(character_name))
+# print(character_name.index("o"))
+# print(character_name.replace("n", "nson"))
+#
+# print(abs(-2.945354))
+# print(pow(3, 2))
+# print(2 + 5.45)
+# print(max(2, 3, 4, 5) + round(2.4))
+#
+# print(floor(3.7) + ceil(2.2))
+# print(sqrt(36))
+#
+# username = input("What's your name?")
+#
+# print("hello " + username + "!")
+#
+# num1 = input("Enter a number: ")
+# num2 = input("Enter another number: ")
+# result = float(num1)+float(num2)
+# print(result)
 
-character_name = "John"
-character_age = 35
 
-
-print(str(character_age))
-
-age = "35"
-
-print("There once was a man named " + character_name + ", ")
-print("he was " + age + " years old. ")
-print("He really liked the name "+character_name+", ")
-print("but didn't like being " + age + " years old.")
-print("      /|")
-print("     / |")
-print("    /  |")
-print("   /___|")
-
-print(character_name.upper())
-print(character_name.isupper())
-print(len(character_name))
-print(character_name.index("o"))
-print(character_name.replace("n", "nson"))
-
-print(abs(-2.945354))
-print(pow(3, 2))
-print(2 + 5.45)
-print(max(2, 3, 4, 5) + round(2.4))
-
-print(floor(3.7) + ceil(2.2))
-print(sqrt(36))
-
-username = input("What's your name?")
-
-print("hello " + username + "!")
-
-num1 = input("Enter a number: ")
-num2 = input("Enter another number: ")
-result = float(num1)+float(num2)
-print(result)
